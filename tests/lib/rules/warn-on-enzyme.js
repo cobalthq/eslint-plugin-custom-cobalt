77f4cfc5df6cd1ad4c9f5f0e1079b244a695d066
Jess Mear <jess.mear@cobalt.io>
2022-01-24 23:05:05 -0700

    update test

--- tests/lib/rules/warn-on-enzyme.js
+++ tests/lib/rules/warn-on-enzyme.js
@@ -2,30 +2,98 @@
  * @fileoverview Remind developers to remove enzyme when a file is updated.
  * @author Jess Lark
  */
-"use strict";
+ "use strict";
 
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+const RuleTester = require("eslint").RuleTester
+const lib = require("../../../lib/rules/warn-on-enzyme")
 
-const rule = require("../../../lib/rules/warn-on-enzyme"),
-  RuleTester = require("eslint").RuleTester;
+RuleTester.setDefaultConfig({
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+})
 
+const ruleTester = new RuleTester()
 
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
-
-const ruleTester = new RuleTester();
-ruleTester.run("warn-on-enzyme", rule, {
+ruleTester.run('removeEnzyme', lib.rules.removeEnzyme, {
+  // any non-enzyme import should pass, whether options are included or not
+  // import mount from something other than enzyme should pass
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      code: "import { foo } from 'bar'",
+      options: [],
+    },
+    {
+      code: "import { mount } from 'notEnzyme'",
+      options: [{ importsToCheckFor: ['enzyme', 'otherThing', 'yetAnother'] }],
+    },
   ],
 
   invalid: [
+    // importing anything from enzyme should fail if there are no options
+    // importing anything from enzyme should fail if there are multiple options
+    // importing anything from a given option should fail
+    // importing items from a multiple given options should fail
+
+    // importing items from enzyme when options that don't include 'emzyme' are given SHOULD fail but probably doesn't
+
     {
       code: "import { mount } from 'enzyme'",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      options: [],
+      errors: [
+        {
+          message: 'Refactor tests to no longer use enzyme',
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: "import { something } from 'enzyme'",
+      options: [
+        { importsToCheckFor: ['enzyme', 'otherThing', 'enzymeRelatedThing'] },
+      ],
+      errors: [
+        {
+          message: 'Refactor tests to no longer use enzyme',
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: "import { something } from 'enzymeRelatedThing'",
+      options: [
+        { importsToCheckFor: ['enzyme', 'otherThing', 'enzymeRelatedThing'] },
+      ],
+      errors: [
+        {
+          message: 'Refactor tests to no longer use enzyme',
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code:
+        "import { something } from 'enzymeRelatedThing' import { something } from 'enzyme'",
+      options: [{ importsToCheckFor: ['enzyme', 'enzymeRelatedThing'] }],
+      errors: [
+        {
+          message: 'Refactor tests to no longer use enzyme',
+          type: 'ImportDeclaration',
+        },
+      ],
+    },
+    {
+      code: "import { something } from 'enzyme'",
+      options: [{ importsToCheckFor: ['otherThing', 'enzymeRelatedThing'] }],
+      errors: [
+        {
+          message: 'Refactor tests to no longer use enzyme',
+          type: 'ImportDeclaration',
+        },
+      ],
     },
   ],
-});
+})
